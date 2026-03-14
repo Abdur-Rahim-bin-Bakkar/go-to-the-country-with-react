23d9234cc648ef5b1b3fb8b8117b09a0635a1f35
webdesignrahim4061 <webdesignrahim4061@gmail.com>
2026-03-14 19:36:50 +0600

    project setup and install needed package and create component file

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+
+import Card from "./componensts/Card"
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Card>
+      
+    </Card>
+      
+    </>
+  )
+}
+
+export default App
